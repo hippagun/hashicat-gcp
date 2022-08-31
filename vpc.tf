ea49292ddda3126644e2ea2f4dc4bebad4a2b1fb
harsha <hippagun@amfam.com>
2022-08-31 19:21:45 +0000

    Added vpc module

--- vpc.tf
+++ vpc.tf
@@ -2,34 +2,17 @@ module "vpc" {
     source  = "terraform-google-modules/network/google"
     version = "~> 4.0"
 
-    project_id   = "<PROJECT ID>"
-    network_name = "example-vpc"
+    project_id   = var.project
+    network_name = "harsha-vpc"
     routing_mode = "GLOBAL"
 
     subnets = [
         {
-            subnet_name           = "subnet-01"
-            subnet_ip             = "10.10.10.0/24"
-            subnet_region         = "us-west1"
-        },
-        {
-            subnet_name           = "subnet-02"
-            subnet_ip             = "10.10.20.0/24"
-            subnet_region         = "us-west1"
-            subnet_private_access = "true"
-            subnet_flow_logs      = "true"
-            description           = "This subnet has a description"
-        },
-        {
-            subnet_name               = "subnet-03"
-            subnet_ip                 = "10.10.30.0/24"
-            subnet_region             = "us-west1"
-            subnet_flow_logs          = "true"
-            subnet_flow_logs_interval = "INTERVAL_10_MIN"
-            subnet_flow_logs_sampling = 0.7
-            subnet_flow_logs_metadata = "INCLUDE_ALL_METADATA"
-        }
-    ]
+    subnet_name   = "harsha-subnet"
+    subnet_ip     = "10.100.10.0/24"
+    subnet_region = var.region
+            }
+        ]
 
     secondary_ranges = {
         subnet-01 = [
@@ -62,10 +45,3 @@ module "vpc" {
 }
 
 
-subnets = [
-  {
-    subnet_name   = "harsha-subnet"
-    subnet_ip     = "10.100.10.0/24"
-    subnet_region = var.region
-  }
-]
